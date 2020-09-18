41768204058c2c4efe92b750a09621abba1291a7
Natasha Aleksandrova <natasha.aleksandrova@zonarsystems.com>
2020-09-17 19:59:44 -0700

    init

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from .config import Settings, get_settings
+
+from fastapi import FastAPI, Depends
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing
+    }
